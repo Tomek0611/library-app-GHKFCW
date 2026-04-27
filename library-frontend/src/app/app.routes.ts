3a03daa8a0fbfd465c5e7e28233e2430dc8840af
pappgyu <167806808+pappgyu@users.noreply.github.com>
2026-04-27 19:27:18 +0200

    Add route for AboutComponent

--- library-frontend/src/app/app.routes.ts
+++ library-frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { BookListComponent } from './components/books/book-list/book-list.component';
 import { BookDetailComponent } from './components/books/book-detail/book-detail.component';
 import { AuthorListComponent } from './components/authors/author-list/author-list.component';
+import { AboutComponent } from './components/shared/about/about.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'books', pathMatch: 'full' },
   { path: 'books', component: BookListComponent },
   { path: 'books/:id', component: BookDetailComponent },
   { path: 'authors', component: AuthorListComponent },
+  { path: 'about', component: AboutComponent },
 ];
