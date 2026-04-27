5c5385f5499d55c665b56b535a04c1014438d3d7
petoka555 <nagy.peter950808@gmail.com>
2026-04-27 17:31:16 +0200

    Update Program.cs

--- LibraryApi/Program.cs
+++ LibraryApi/Program.cs
@@ -1,5 +1,6 @@
 using LibraryApi.Services;
 using LibraryApi.Settings;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,18 @@ builder.Services.AddSingleton<AuthorService>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new()
+    {
+        Title = "Library API",
+        Version = "v1",
+        Description = "Online Könyvtár REST API – könyvek és szerzők kezelése"
+    });
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 builder.Services.AddCors(options =>
 {
@@ -22,7 +34,11 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Library API v1");
+    c.RoutePrefix = "swagger";
+});
 app.UseCors();
 app.UseAuthorization();
 app.MapControllers();
